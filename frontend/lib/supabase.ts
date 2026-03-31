1a0474a28884b9ca3f6015bc68724985246c3f96
Bernardo Jimenez <berna2103@yahoo.com>
2026-03-31 09:40:18 -0500

    subabase improvements

--- frontend/lib/supabase.ts
+++ frontend/lib/supabase.ts
@@ -1,21 +1,6 @@
-import { createClient, SupabaseClient } from '@supabase/supabase-js';
+import { createBrowserClient } from '@supabase/ssr';
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || '';
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || '';
 
-// Prevent multiple instances of Supabase client in development
-const globalForSupabase = global as unknown as { supabase: SupabaseClient };
-
-export const supabase =
-  globalForSupabase.supabase || 
-  createClient(supabaseUrl, supabaseKey, {
-    auth: {
-      storageKey: 'sb-auth-token', // Ensure consistent key
-      persistSession: true,
-      autoRefreshToken: true,
-      detectSessionInUrl: true,
-      // If the lock error persists, you can try setting flowType or custom storage
-    }
-  });
-
-if (process.env.NODE_ENV !== 'production') globalForSupabase.supabase = supabase;
+export const supabase = createBrowserClient(supabaseUrl, supabaseKey);
